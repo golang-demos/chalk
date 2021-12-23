c1fad45b249bc38924d22c269a08ef1cce16f29e
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-24 05:22:02 +0530

    Added string parser

--- chalk.go
+++ chalk.go
@@ -1,7 +1,17 @@
 package chalk
 
+import "fmt"
+
 type Color struct {
 	color   int
 	style   int
 	bgcolor int
+}
+
+func (c Color) String() string {
+	styleText := fmt.Sprintf("\u001b[%dm", c.color)
+	if c.style > 0 {
+		styleText = fmt.Sprintf("\u001b[%d;%dm", c.style, c.color)
+	}
+	return fmt.Sprintf("\u001b[%dm", c.bgcolor) + styleText
 }
