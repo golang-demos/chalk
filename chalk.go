f1d809644b22f46e8c49cb5c954f7130b73b8275
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-24 05:23:59 +0530

    Added Red Text Color

--- chalk.go
+++ chalk.go
@@ -1,4 +1,4 @@
-package chalk
+package main
 
 import "fmt"
 
@@ -14,4 +14,32 @@ func (c Color) String() string {
 		styleText = fmt.Sprintf("\u001b[%d;%dm", c.style, c.color)
 	}
 	return fmt.Sprintf("\u001b[%dm", c.bgcolor) + styleText
+}
+
+// Colors
+func getColorIntensity(intensity []bool) (intensityVal int) {
+	intensityVal = 30
+	if len(intensity) > 0 && intensity[0] {
+		intensityVal = 90
+	}
+	return
+}
+func (c *Color) Red(makeIntense ...bool) *Color {
+	c.color = getColorIntensity(makeIntense) + 1
+	return c
+}
+
+var (
+	// Text Colors
+	Red = (&Color{}).Red
+)
+
+// For all reset. BackgroundColor and TextColor
+func Reset() string {
+	return fmt.Sprintf("\u001b[0;%dm", 0)
+}
+
+func main() {
+
+	fmt.Println("--", Red(), "Red + Regular", Reset(), "--")
 }
