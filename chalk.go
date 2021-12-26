a76bebac1e6a37e14ecee32b0c9d39ff101d0403
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-27 01:52:29 +0530

    WIP: Debuging background color issue

--- chalk.go
+++ chalk.go
@@ -13,10 +13,11 @@ type Color struct {
 var LIBRARY_DEVELOPER_MODE = false
 
 func (c Color) String() string {
+	fmt.Println("\n", c.color, c.style, c.bgcolor)
 	if LIBRARY_DEVELOPER_MODE {
 		return fmt.Sprintf("\\u001b[%dm", c.bgcolor) + fmt.Sprintf("\\u001b[%d;%dm", c.style, c.color)
 	} else {
-		styleText := fmt.Sprintf("\u001b[%dm", c.color)
+		styleText := fmt.Sprintf("\u001b[0;%dm", c.color)
 		if c.style > 0 {
 			styleText = fmt.Sprintf("\u001b[%d;%dm", c.style, c.color)
 		}
@@ -37,6 +38,7 @@ func (c *Color) Black(makeIntense ...bool) *Color {
 	return c
 }
 func (c *Color) Red(makeIntense ...bool) *Color {
+	// log.Println(">", c.color, c.style, c.bgcolor)
 	c.color = getColorIntensity(makeIntense) + 1
 	return c
 }
@@ -147,38 +149,50 @@ func (c *Color) BgWhite(makeIntense ...bool) *Color {
 
 // For all reset. BackgroundColor and TextColor
 func Reset() string {
-	return fmt.Sprintf("\033[%dm", 0)
+	if LIBRARY_DEVELOPER_MODE {
+		return fmt.Sprintf("\\033[0;%dm\\033[%dm", 0, 49)
+	} else {
+		return fmt.Sprintf("\033[0;%dm\033[%dm", 0, 49)
+	}
+}
+
+func newColor() *Color {
+	var c Color
+	c.color = 0
+	c.style = 0
+	c.bgcolor = 0
+	return &c
 }
 
 var (
 	// Text Colors
-	Black  = (&Color{}).Black
-	Red    = (&Color{}).Red
-	Green  = (&Color{}).Green
-	Orange = (&Color{}).Orange
-	Yellow = (&Color{}).Yellow
-	Blue   = (&Color{}).Blue
-	Purple = (&Color{}).Purple
-	Violet = (&Color{}).Violet
-	Cyan   = (&Color{}).Cyan
-	White  = (&Color{}).White
+	Black  = func() *Color { return newColor().Black() }
+	Red    = func() *Color { return newColor().Red() }
+	Green  = func() *Color { return newColor().Green() }
+	Orange = func() *Color { return newColor().Orange() }
+	Yellow = func() *Color { return newColor().Yellow() }
+	Blue   = func() *Color { return newColor().Blue() }
+	Purple = func() *Color { return newColor().Purple() }
+	Violet = func() *Color { return newColor().Violet() }
+	Cyan   = func() *Color { return newColor().Cyan() }
+	White  = func() *Color { return newColor().White() }
 
 	// Text Styles
-	Bold          = (&Color{}).Bold
-	Dim           = (&Color{}).Dim
-	Italic        = (&Color{}).Italic
-	Underline     = (&Color{}).Underline
-	Inverse       = (&Color{}).Inverse
-	Hidden        = (&Color{}).Hidden
-	Strikethrough = (&Color{}).Strikethrough
+	Bold          = func() *Color { return newColor().Bold() }
+	Dim           = func() *Color { return newColor().Dim() }
+	Italic        = func() *Color { return newColor().Italic() }
+	Underline     = func() *Color { return newColor().Underline() }
+	Inverse       = func() *Color { return newColor().Inverse() }
+	Hidden        = func() *Color { return newColor().Hidden() }
+	Strikethrough = func() *Color { return newColor().Strikethrough() }
 
 	// Background Colors
-	BgBlack  = (&Color{}).BgBlack
-	BgRed    = (&Color{}).BgRed
-	BgGreen  = (&Color{}).BgGreen
-	BgYellow = (&Color{}).BgYellow
-	BgBlue   = (&Color{}).BgBlue
-	BgPurple = (&Color{}).BgPurple
-	BgCyan   = (&Color{}).BgCyan
-	BgWhite  = (&Color{}).BgWhite
+	BgBlack  = func() *Color { return newColor().BgBlack() }
+	BgRed    = func() *Color { return newColor().BgRed() }
+	BgGreen  = func() *Color { return newColor().BgGreen() }
+	BgYellow = func() *Color { return newColor().BgYellow() }
+	BgBlue   = func() *Color { return newColor().BgBlue() }
+	BgPurple = func() *Color { return newColor().BgPurple() }
+	BgCyan   = func() *Color { return newColor().BgCyan() }
+	BgWhite  = func() *Color { return newColor().BgWhite() }
 )
