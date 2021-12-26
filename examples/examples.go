c28647eaaef1309deae47759c2df0aea4f4edffb
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-27 01:01:30 +0530

    Added some examples

--- examples/examples.go
+++ examples/examples.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	// chalk.LIBRARY_DEVELOPER_MODE = true
+
 	fmt.Println("--", chalk.Red(), "Red + Regular", chalk.Reset(), "--")
 	fmt.Println("--", chalk.Red().Bold(), "Red + Bold", chalk.Reset(), "--")
 	fmt.Println("--", chalk.Red().Italic(), "Red + Italic", chalk.Reset(), "--")
@@ -16,12 +18,14 @@ func main() {
 	fmt.Println("--", chalk.White().Bold().BgRed(), "White + Bold + BgRed", chalk.Reset(), "--")
 	fmt.Println("--", chalk.White().Bold().BgRed(true), "White + Bold + BgRed(I)", chalk.Reset(), "--")
 
-	fmt.Println("--", chalk.Red().BgWhite(true), "Red + Regular + BgWhite(I)", chalk.Reset(), "--")
-	fmt.Println("--", chalk.Red(true).BgWhite(true), "Red(I) + Regular + BgWhite(I)", chalk.Reset(), "--")
+	fmt.Println("--", chalk.Red().BgBlue(), "Red + Regular + BgBlue(I)", chalk.Reset(), "--")
+	fmt.Println("--", chalk.Red().BgBlue(), "Red(I) + Regular + BgBlue(I)", chalk.Reset(), "--")
 
 	fmt.Println("--", chalk.Black().Italic().BgGreen(), "Black + Italic + BgGreen", chalk.Reset(), "--")
 	fmt.Println("--", chalk.Black().Italic().BgGreen(true), "Black + Italic + BgGreen(I)", chalk.Reset(), "--")
 
 	fmt.Println("--", chalk.Cyan().BgBlack(true), "Cyan + Regular + BgBlack(I)", chalk.Reset(), "--")
 	fmt.Println("--", chalk.Cyan(true).BgBlack(true), "Cyan(I) + Regular + BgBlack(I)", chalk.Reset(), "--")
+
+	fmt.Println(chalk.Red().Bold(), "My Name is ", chalk.Green(), "Vinay", chalk.Reset())
 }
