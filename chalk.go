4b297f184db4e3ad1de340a546daf4d4e15ea3a6
Vinay Jeurkar <vjeurkar03@gmail.com>
2022-06-20 23:55:09 +0530

    Added support for printable text

--- chalk.go
+++ chalk.go
@@ -5,10 +5,15 @@ import (
 	"strings"
 )
 
+type PrintableText interface {
+	String() string
+}
+
 type Color struct {
 	color   int
 	style   int
 	bgcolor int
+	text    []interface{}
 }
 
 func (c Color) String() string {
@@ -27,170 +32,226 @@ func (c Color) String() string {
 
 	ext = strings.Join(parts[:], ";")
 
-	return "\u001b[" + ext + "m"
+	returnString := "\u001b[" + ext + "m"
+
+	if len(c.text) > 0 {
+		for _, item := range c.text {
+			switch f := item.(type) {
+			default:
+				returnString += fmt.Sprint(f)
+			}
+		}
+		returnString += Reset()
+	}
+
+	return returnString
+}
+func (c *Color) SetText(msgs []interface{}) {
+	if len(msgs) > 0 {
+		c.text = msgs
+	}
 }
 
 // Colors
-func (c *Color) Black() *Color {
+func (c *Color) Black(msgs ...interface{}) *Color {
 	c.color = 30
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Red() *Color {
+func (c *Color) Red(msgs ...interface{}) *Color {
 	c.color = 31
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Green() *Color {
+func (c *Color) Green(msgs ...interface{}) *Color {
 	c.color = 32
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Yellow() *Color {
+func (c *Color) Yellow(msgs ...interface{}) *Color {
 	c.color = 33
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Blue() *Color {
+func (c *Color) Blue(msgs ...interface{}) *Color {
 	c.color = 34
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Magenta() *Color {
+func (c *Color) Magenta(msgs ...interface{}) *Color {
 	c.color = 35
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Cyan() *Color {
+func (c *Color) Cyan(msgs ...interface{}) *Color {
 	c.color = 36
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) White() *Color {
+func (c *Color) White(msgs ...interface{}) *Color {
 	c.color = 37
+	c.SetText(msgs)
 	return c
 }
 
-func (c *Color) BlackLight() *Color {
+func (c *Color) BlackLight(msgs ...interface{}) *Color {
 	c.color = 90
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) RedLight() *Color {
+func (c *Color) RedLight(msgs ...interface{}) *Color {
 	c.color = 91
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) GreenLight() *Color {
+func (c *Color) GreenLight(msgs ...interface{}) *Color {
 	c.color = 92
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) YellowLight() *Color {
+func (c *Color) YellowLight(msgs ...interface{}) *Color {
 	c.color = 93
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BlueLight() *Color {
+func (c *Color) BlueLight(msgs ...interface{}) *Color {
 	c.color = 94
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) MagentaLight() *Color {
+func (c *Color) MagentaLight(msgs ...interface{}) *Color {
 	c.color = 95
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) CyanLight() *Color {
+func (c *Color) CyanLight(msgs ...interface{}) *Color {
 	c.color = 96
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) WhiteLight() *Color {
+func (c *Color) WhiteLight(msgs ...interface{}) *Color {
 	c.color = 97
+	c.SetText(msgs)
 	return c
 }
 
 // Text Styles
-func (c *Color) Bold() *Color {
+func (c *Color) Bold(msgs ...interface{}) *Color {
 	c.style = 1
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Dim() *Color {
+func (c *Color) Dim(msgs ...interface{}) *Color {
 	c.style = 2
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Italic() *Color {
+func (c *Color) Italic(msgs ...interface{}) *Color {
 	c.style = 3
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Underline() *Color {
+func (c *Color) Underline(msgs ...interface{}) *Color {
 	c.style = 4
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Inverse() *Color {
+func (c *Color) Inverse(msgs ...interface{}) *Color {
 	c.style = 7
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Hidden() *Color {
+func (c *Color) Hidden(msgs ...interface{}) *Color {
 	c.style = 8
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) Strikethrough() *Color {
+func (c *Color) Strikethrough(msgs ...interface{}) *Color {
 	c.style = 9
+	c.SetText(msgs)
 	return c
 }
 
 // Background Colors
-func (c *Color) BgBlack() *Color {
+func (c *Color) BgBlack(msgs ...interface{}) *Color {
 	c.bgcolor = 40
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgRed() *Color {
+func (c *Color) BgRed(msgs ...interface{}) *Color {
 	c.bgcolor = 41
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgGreen() *Color {
+func (c *Color) BgGreen(msgs ...interface{}) *Color {
 	c.bgcolor = 42
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgYellow() *Color {
+func (c *Color) BgYellow(msgs ...interface{}) *Color {
 	c.bgcolor = 43
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgBlue() *Color {
+func (c *Color) BgBlue(msgs ...interface{}) *Color {
 	c.bgcolor = 44
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgMagenta() *Color {
+func (c *Color) BgMagenta(msgs ...interface{}) *Color {
 	c.bgcolor = 45
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgCyan() *Color {
+func (c *Color) BgCyan(msgs ...interface{}) *Color {
 	c.bgcolor = 46
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgWhite() *Color {
+func (c *Color) BgWhite(msgs ...interface{}) *Color {
 	c.bgcolor = 47
+	c.SetText(msgs)
 	return c
 }
 
-func (c *Color) BgBlackLight() *Color {
+func (c *Color) BgBlackLight(msgs ...interface{}) *Color {
 	c.bgcolor = 100
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgRedLight() *Color {
+func (c *Color) BgRedLight(msgs ...interface{}) *Color {
 	c.bgcolor = 101
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgGreenLight() *Color {
+func (c *Color) BgGreenLight(msgs ...interface{}) *Color {
 	c.bgcolor = 102
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgYellowLight() *Color {
+func (c *Color) BgYellowLight(msgs ...interface{}) *Color {
 	c.bgcolor = 103
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgBlueLight() *Color {
+func (c *Color) BgBlueLight(msgs ...interface{}) *Color {
 	c.bgcolor = 104
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgMagentaLight() *Color {
+func (c *Color) BgMagentaLight(msgs ...interface{}) *Color {
 	c.bgcolor = 105
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgCyanLight() *Color {
+func (c *Color) BgCyanLight(msgs ...interface{}) *Color {
 	c.bgcolor = 106
+	c.SetText(msgs)
 	return c
 }
-func (c *Color) BgWhiteLight() *Color {
+func (c *Color) BgWhiteLight(msgs ...interface{}) *Color {
 	c.bgcolor = 107
+	c.SetText(msgs)
 	return c
 }
 
@@ -209,50 +270,50 @@ func newColor() *Color {
 
 var (
 	// Text Colors
-	Black   = func() *Color { return newColor().Black() }
-	Red     = func() *Color { return newColor().Red() }
-	Green   = func() *Color { return newColor().Green() }
-	Yellow  = func() *Color { return newColor().Yellow() }
-	Blue    = func() *Color { return newColor().Blue() }
-	Magenta = func() *Color { return newColor().Magenta() }
-	Cyan    = func() *Color { return newColor().Cyan() }
-	White   = func() *Color { return newColor().White() }
+	Black   = func(msgs ...interface{}) *Color { return newColor().Black(msgs...) }
+	Red     = func(msgs ...interface{}) *Color { return newColor().Red(msgs...) }
+	Green   = func(msgs ...interface{}) *Color { return newColor().Green(msgs...) }
+	Yellow  = func(msgs ...interface{}) *Color { return newColor().Yellow(msgs...) }
+	Blue    = func(msgs ...interface{}) *Color { return newColor().Blue(msgs...) }
+	Magenta = func(msgs ...interface{}) *Color { return newColor().Magenta(msgs...) }
+	Cyan    = func(msgs ...interface{}) *Color { return newColor().Cyan(msgs...) }
+	White   = func(msgs ...interface{}) *Color { return newColor().White(msgs...) }
 
 	// Text Light Colors
-	BlackLight   = func() *Color { return newColor().BlackLight() }
-	RedLight     = func() *Color { return newColor().RedLight() }
-	GreenLight   = func() *Color { return newColor().GreenLight() }
-	YellowLight  = func() *Color { return newColor().YellowLight() }
-	BlueLight    = func() *Color { return newColor().BlueLight() }
-	MagentaLight = func() *Color { return newColor().MagentaLight() }
-	CyanLight    = func() *Color { return newColor().CyanLight() }
-	WhiteLight   = func() *Color { return newColor().WhiteLight() }
+	BlackLight   = func(msgs ...interface{}) *Color { return newColor().BlackLight(msgs...) }
+	RedLight     = func(msgs ...interface{}) *Color { return newColor().RedLight(msgs...) }
+	GreenLight   = func(msgs ...interface{}) *Color { return newColor().GreenLight(msgs...) }
+	YellowLight  = func(msgs ...interface{}) *Color { return newColor().YellowLight(msgs...) }
+	BlueLight    = func(msgs ...interface{}) *Color { return newColor().BlueLight(msgs...) }
+	MagentaLight = func(msgs ...interface{}) *Color { return newColor().MagentaLight(msgs...) }
+	CyanLight    = func(msgs ...interface{}) *Color { return newColor().CyanLight(msgs...) }
+	WhiteLight   = func(msgs ...interface{}) *Color { return newColor().WhiteLight(msgs...) }
 
 	// Text Styles
-	Bold          = func() *Color { return newColor().Bold() }
-	Dim           = func() *Color { return newColor().Dim() }
-	Italic        = func() *Color { return newColor().Italic() }
-	Underline     = func() *Color { return newColor().Underline() }
-	Inverse       = func() *Color { return newColor().Inverse() }
-	Hidden        = func() *Color { return newColor().Hidden() }
-	Strikethrough = func() *Color { return newColor().Strikethrough() }
+	Bold          = func(msgs ...interface{}) *Color { return newColor().Bold(msgs...) }
+	Dim           = func(msgs ...interface{}) *Color { return newColor().Dim(msgs...) }
+	Italic        = func(msgs ...interface{}) *Color { return newColor().Italic(msgs...) }
+	Underline     = func(msgs ...interface{}) *Color { return newColor().Underline(msgs...) }
+	Inverse       = func(msgs ...interface{}) *Color { return newColor().Inverse(msgs...) }
+	Hidden        = func(msgs ...interface{}) *Color { return newColor().Hidden(msgs...) }
+	Strikethrough = func(msgs ...interface{}) *Color { return newColor().Strikethrough(msgs...) }
 
 	// Background Colors
-	BgBlack   = func() *Color { return newColor().BgBlack() }
-	BgRed     = func() *Color { return newColor().BgRed() }
-	BgGreen   = func() *Color { return newColor().BgGreen() }
-	BgYellow  = func() *Color { return newColor().BgYellow() }
-	BgBlue    = func() *Color { return newColor().BgBlue() }
-	BgMagenta = func() *Color { return newColor().BgMagenta() }
-	BgCyan    = func() *Color { return newColor().BgCyan() }
-	BgWhite   = func() *Color { return newColor().BgWhite() }
+	BgBlack   = func(msgs ...interface{}) *Color { return newColor().BgBlack(msgs...) }
+	BgRed     = func(msgs ...interface{}) *Color { return newColor().BgRed(msgs...) }
+	BgGreen   = func(msgs ...interface{}) *Color { return newColor().BgGreen(msgs...) }
+	BgYellow  = func(msgs ...interface{}) *Color { return newColor().BgYellow(msgs...) }
+	BgBlue    = func(msgs ...interface{}) *Color { return newColor().BgBlue(msgs...) }
+	BgMagenta = func(msgs ...interface{}) *Color { return newColor().BgMagenta(msgs...) }
+	BgCyan    = func(msgs ...interface{}) *Color { return newColor().BgCyan(msgs...) }
+	BgWhite   = func(msgs ...interface{}) *Color { return newColor().BgWhite(msgs...) }
 
-	BgBlackLight   = func() *Color { return newColor().BgBlackLight() }
-	BgRedLight     = func() *Color { return newColor().BgRedLight() }
-	BgGreenLight   = func() *Color { return newColor().BgGreenLight() }
-	BgYellowLight  = func() *Color { return newColor().BgYellowLight() }
-	BgBlueLight    = func() *Color { return newColor().BgBlueLight() }
-	BgMagentaLight = func() *Color { return newColor().BgMagentaLight() }
-	BgCyanLight    = func() *Color { return newColor().BgCyanLight() }
-	BgWhiteLight   = func() *Color { return newColor().BgWhiteLight() }
+	BgBlackLight   = func(msgs ...interface{}) *Color { return newColor().BgBlackLight(msgs...) }
+	BgRedLight     = func(msgs ...interface{}) *Color { return newColor().BgRedLight(msgs...) }
+	BgGreenLight   = func(msgs ...interface{}) *Color { return newColor().BgGreenLight(msgs...) }
+	BgYellowLight  = func(msgs ...interface{}) *Color { return newColor().BgYellowLight(msgs...) }
+	BgBlueLight    = func(msgs ...interface{}) *Color { return newColor().BgBlueLight(msgs...) }
+	BgMagentaLight = func(msgs ...interface{}) *Color { return newColor().BgMagentaLight(msgs...) }
+	BgCyanLight    = func(msgs ...interface{}) *Color { return newColor().BgCyanLight(msgs...) }
+	BgWhiteLight   = func(msgs ...interface{}) *Color { return newColor().BgWhiteLight(msgs...) }
 )
